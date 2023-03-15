bb1ba52305a3762bf810954aae94d216e235c4d3
Patchara KL <patchara.pck@gmail.com>
2023-03-15 18:14:13 +0700

    fuzzy now functioning

--- fuzzy.py
+++ fuzzy.py
@@ -18,7 +18,7 @@ def fuzzy_cal(self_hp, work_hp, co_worker_hp):
     workspace_happiness['saddest'] = fuzz.trapmf(workspace_happiness.universe, [0, 0, 20, 50])
     workspace_happiness['normal'] = fuzz.trimf(workspace_happiness.universe, [20, 50, 80])
     workspace_happiness['happiest'] = fuzz.trapmf(workspace_happiness.universe, [50, 90, 100, 100])
-    
+
     co_worker_happiness['saddest'] = fuzz.trapmf(co_worker_happiness.universe, [0, 0, 20, 50])
     co_worker_happiness['normal'] = fuzz.trimf(co_worker_happiness.universe, [20, 50, 80])
     co_worker_happiness['happiest'] = fuzz.trapmf(co_worker_happiness.universe, [50, 90, 100, 100])
@@ -27,7 +27,7 @@ def fuzzy_cal(self_hp, work_hp, co_worker_hp):
     score['low'] = fuzz.trapmf(score.universe, [0, 0, 20, 50])
     score['medium'] = fuzz.trimf(score.universe, [20, 50, 80])
     score['high'] = fuzz.trapmf(score.universe, [50, 90, 100, 100])
-    score.view()
+
     # Rules
     rule1 = ctrl.Rule(self_happiness['saddest'] | workspace_happiness['saddest'] | co_worker_happiness['saddest'], score['low'])
     rule2 = ctrl.Rule(workspace_happiness['normal'] | self_happiness['normal'] | co_worker_happiness['normal'], score['medium'])
@@ -38,9 +38,9 @@ def fuzzy_cal(self_hp, work_hp, co_worker_hp):
     scoring = ctrl.ControlSystemSimulation(scoring_ctrl)
     # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
     # Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
-    scoring.input['self-happiness'] = 100
-    scoring.input['workspace-happiness'] = 100
-    scoring.input['co_worker_happiness'] = 100
+    scoring.input['self-happiness'] = self_hp
+    scoring.input['workspace-happiness'] = work_hp
+    scoring.input['co_worker_happiness'] = co_worker_hp
 
     # Crunch the numbers
     scoring.compute()
